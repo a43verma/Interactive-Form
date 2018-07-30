7b73ffc6e72772c9253622f9ecd78a71e09567f4
Akash Verma <akash_verma@outlook.com>
2018-07-30 06:04:08 -0400

    Job Role Other Input Works

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+// Page load starts with focus on name input
 $('#name').focus();
+
+// Creates input field
+function createInput () {
+  const newLi = document.createElement("input");
+  $(newLi).attr({
+    type: 'text',
+    id: 'job',
+    name: 'job_role',
+    placeholder: 'Your job role'
+  })
+  return newLi;
+}
+
+$('.basic').append(createInput);
+$('#job').hide();
+
+// Acknowledges the "other" job role
+$("#title").on("change", function () {
+  if (this.value === "other") {
+    $('#job').show();
+  } else {
+    $('#job').hide();
+  }
+});
+
+})
